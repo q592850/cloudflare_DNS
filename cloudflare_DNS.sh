fc9eed2edd734a2f547b5621386f1ed62e38bcb1
中国凯易网络科技有限公司 <36805246+q592850@users.noreply.github.com>
2023-04-15 14:44:14 +0800

    Update cloudflare_DNS.sh

--- cloudflare_DNS.sh
+++ cloudflare_DNS.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Cloudflare API 凭据
-auth_email=""
-auth_key=""
+auth_email="############"
+auth_key="############"
 
 # 域名信息
-name=""  # 主域名
-name_2=""  # 二级域名
-content_cm1=""  # 端口开启时的 CNAME 记录值
-content_cm2=""  # 端口关闭时的 CNAME 记录值
+name="############"  # 主域名
+name_2="############"  # 二级域名
+content_cm1="############"  # 端口开启时的 CNAME 记录值
+content_cm2="############"  # 端口关闭时的 CNAME 记录值
 
 # 获取区域 ID
 zone=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones" \
@@ -37,17 +37,19 @@ fi
 echo "记录 ID：$record"
 
 # 设置初始 CNAME 记录值
-initial_content="$content_cm1"
+content=""
 
 # 持续监视目标端口
 while true; do
   # 检查目标端口是否开启
-  if nc -w 2 cm1.blackpink520.cyou 49803 < /dev/null 2>&1 | grep -q Connected; then
+  if nc -z -w 2 cm1.moni.cyou 49803 > /dev/null 2>&1; then
     # 如果目标端口开启，则将 CNAME 记录值设置为初始值
-    content="$initial_content"
+    content="$content_cm1"
+    echo "主入口：${content}"
   else
     # 如果目标端口关闭，则将 CNAME 记录值设置为备份值
     content="$content_cm2"
+    echo "备入口：${content}"
   fi
 
   # 使用新的 CNAME 记录值更新 DNS 记录
@@ -58,11 +60,12 @@ while true; do
   --data "{\"type\":\"CNAME\",\"name\":\"${name_2}\",\"content\":\"${content}\",\"ttl\":120,\"proxied\":false}" | jq -r '.success')
   
   if [[ $response == "true" ]]; then
-    echo "DNS 记录更新成功。"
+    echo "DNS 记录更新成功。${content}"
   else
     echo "更新 DNS 记录时出错。"
   fi
 
   # 等待 5 分钟
   sleep 300
-done
+
+ done
